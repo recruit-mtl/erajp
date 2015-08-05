dc68f35c4f4df87b05df6d72cc277d0cee73e221
kasajei <sevenbluesea.yk@gmail.com>
2015-08-05 18:59:52 +0900

    refactor setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ setup(
     name='erajp',
     version='0.0.1',
     description='Convert datetime to Japanese era',
-    author='@kasajei',
+    author='kasajei',
     author_email='kasajei@me.com',
-    url='http://mtl.recruit.co.jp/',
+    url='https://github.com/recruit-mtl/erajp',
     packages=['erajp'],
     install_requires = [],
-    tests_require=['nose', 'coverage'],
+    extras_require = {
+        "test":['nose', 'coverage']
+    }
 )
